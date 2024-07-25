03830d82fcbf0961acf3cd3b42e98e02251223b4
Henrique Pagani <henriqueptho44@hotmail.com>
2024-07-25 18:32:54 -0300

    Page Inicial pronta

--- script.js
+++ script.js
@@ -16,12 +16,14 @@ $(document).ready(function() {
 
     // Inicializa o Owl Carousel
     $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,              // Loop infinito
-        margin: 10,              // Margem entre os itens
-        nav: true
-        // autoplay: true,         
-        // autoplayTimeout: 3000,  
-        // autoplayHoverPause: true 
-    });
+        items: 1, // Quantidade de itens visíveis
+        loop: true, // Habilitar looping
+        margin: 10, // Espaço entre os itens
+        nav: true, // Habilitar navegação
+        dots: true, // Habilitar indicadores
+        autoplay: true, // Habilitar autoplay
+        autoplayTimeout: 3000, // Intervalo do autoplay
+        autoplayHoverPause: true, // Pausar autoplay ao passar o mouse
+        navText: ["<div class='nav-btn prev-slide'>&#10094;</div>", "<div class='nav-btn next-slide'>&#10095;</div>"]
+      });
 });
